377b425623ec3e0a2ebbb7721252f1cc5098b8fb
NedosypI <irinanedosyp@gmx.de>
2022-07-08 10:32:50 +0200

    JS ist eine untypisierte Sprache|untyped|

--- script_01.js
+++ script_01.js
@@ -16,11 +16,31 @@
 
 /***** 02 Deklaration + Wertzuweisung *****/
 
-let firstName; // Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung
-console.log(firstName); // Ausgabe
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
 
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log(familyName); // Ausgabe
-console.log(firstName + " " + familyName); // Ausgabe
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log(familyName); // Ausgabe
+// console.log(firstName + " " + familyName); // Ausgabe
 
+/***** 03 Deklaration + Wertzuweisung II *****/
+// Javascript ist eine untypisierte Sprache, wir können hier ohne Probleme Datentypen austauschen
+
+// let firstName,familyName;
+// firstName =prompt("Bitte Vornamen eingeben")
+// familyName =prompt("Bitte Nachnamen eingeben") // any = untypisierte Sprache, hier kann nachträglich jeglicher Datentypen reingeladen werden
+// console.log(firstName + " " + familyName); // Ausgabe
+
+
+// JS ist eine untypisierte Sprache|untyped|
+// wir können hier ohne Probleme Datentypen austauschen
+
+let test;
+test = "Wazup";
+test = 2; // das 1.test wurde mit dem 2. überschrieben 
+test = true;
+
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
