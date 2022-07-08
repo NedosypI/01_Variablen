4ef12e7523af34d2c7d37f02da7b1b430cc5445c
NedosypI <irinanedosyp@gmx.de>
2022-07-08 09:15:26 +0200

    02 Deklaration + Wertzuweisung

--- script_01.js
+++ script_01.js
@@ -15,3 +15,12 @@
 // console.log(firstName);
 
 /***** 02 Deklaration + Wertzuweisung *****/
+
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log(familyName); // Ausgabe
+console.log(firstName + " " + familyName); // Ausgabe
+
